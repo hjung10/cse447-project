9f6e73044beaf369f9cd8b73558357b5a7c2a2b5
Hayoung Jung <hjung10@cs.washington.edu>
2022-02-28 20:17:17 -0800

    Updated the saving and loading portion

--- src/myprogram.py
+++ src/myprogram.py
@@ -14,6 +14,7 @@ from datasets import load_dataset
 import nltk
 from nltk import word_tokenize
 from tqdm import tqdm
+import json
 
 torch.manual_seed(1)
 
@@ -23,7 +24,9 @@ def load_training_data():
         # x = 'Happy New Yea That’s one small ste one giant leap for mankin'
         # x = set(x)
         # char_to_idx = {c: j for j, c in enumerate(x)}
-        char_to_idx = {"'": 0}
+
+        # ~ represents UNK character
+        char_to_idx = {"'": 0, "<UNK>":1}
         for seq in train_data:
             for c in seq:
                 if c not in char_to_idx:
@@ -33,21 +36,23 @@ def load_training_data():
     # your code here
     # this particular model doesn't train
     # adding more languages other than English to be a little more robust/generalizable
-    top_languages = ['english', 'spanish', 'chinese_simplified', 'hindi', 'arabic']
-    # lang_datasets = [load_dataset('csebuetnlp/xlsum', lang, split='train') for lang in top_languages]
+    top_languages = ['english', 'spanish', 'chinese_simplified', 'russian'] # 'hindi', 'arabic'
+    lang_datasets = [load_dataset('csebuetnlp/xlsum', lang, split='train') for lang in top_languages]
 
     # manually reading data because load_dataset caused disk quota exceeded error
+    """
     lang_datasets = []
     for lang in ['english']:
         path = Path('./XLSum_complete_v2.0/{}_train.jsonl'.format(lang))
         with open(path, 'rb') as f:
             json_list = [literal_eval(line.decode('utf8')) for line in f]
             lang_datasets.append(json_list)
+    """
 
     sentences = []
     max_len = 10000
     # TODO: set this to be dataset.num_rows when we want to train on a larger set of data
-    num_rows = 200  # 2
+    num_rows = 1000  # 2
     for dataset in lang_datasets:
         for i in range(num_rows):
             sentences.append(dataset[i]['text'][:max_len])
@@ -91,14 +96,35 @@ def write_pred(preds, fname):
             f.write('{}\n'.format(p))
 
 
-def save(model, work_dir):
+def save(model, char_to_idx, idx_to_char, work_dir):
     torch.save(model.state_dict(), os.path.join(work_dir, 'model.checkpoint'))
-
+    dictionaries = {
+            'idx_to_char': idx_to_char,
+            'char_to_idx': char_to_idx
+            # 'unigrams': self.unigrams_context_freq,
+            # 'bigrams': self.bigrams_context_freq,
+            # 'trigrams': self.trigrams_context_freq
+        }
+    with open(os.path.join(work_dir, 'model.dictionary'), 'w') as output_json:
+        json.dump(dictionaries, output_json)
 
 def load(model, work_dir):
     # checkpoint = torch.load(work_dir, map_location='cpu')
     # model.load_state_dict(checkpoint)
     model.load_state_dict(torch.load(os.path.join(work_dir, 'model.checkpoint')))
+
+def load_dictionary(work_dir):
+    path = Path(os.path.join(work_dir, 'model.dictionary'))
+    with open(path, 'rb') as f:
+        data = json.load(f)
+
+    print("starting conversion from json")
+    idx_to_char = dict()
+    char_to_idx = data["char_to_idx"]
+    for k, v in data["idx_to_char"].items():
+        idx_to_char[int(k)] = v 
+    print("finished conversion from json")
+    return idx_to_char, char_to_idx
 
 
 # TODO: still in progress
@@ -192,7 +218,7 @@ def evaluate(test_data, model, char_to_idx, idx_to_char):
     preds_list = []
     inputs = []
     for i, sentence in enumerate(test_data):
-        inputs.append([char_to_idx[c] for c in sentence])
+        inputs.append([char_to_idx[c] if c in char_to_idx else char_to_idx["<UNK>"] for c in sentence])
 
     model.eval()
     with torch.no_grad():
@@ -201,10 +227,14 @@ def evaluate(test_data, model, char_to_idx, idx_to_char):
             scores = model(input)
             prob = F.softmax(scores[-1], dim=0).data
             print('char_indices:', torch.topk(prob, 3, dim=0))
+
             char_indices = torch.topk(prob, 3, dim=0)[1]
             preds_chars = ''
             for c_idx in char_indices:
-                preds_chars += idx_to_char[c_idx.item()]
+                index = c_idx.item()
+                if index == 1:
+                    index = random.randint(0, len(char_to_idx) - 1)
+                preds_chars += idx_to_char[index]
             preds_list.append(preds_chars)
 
     return preds_list
@@ -212,8 +242,9 @@ def evaluate(test_data, model, char_to_idx, idx_to_char):
 
 if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('mode', choices=('train', 'test'), help='what to run')
+    parser.add_argument('mode', choices=('train', 'dev', 'test'), help='what to run')
     parser.add_argument('--work_dir', help='where to save', default='work')
+    
     parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
     parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
     args = parser.parse_args()
@@ -245,8 +276,20 @@ if __name__ == '__main__':
         print('Training')
         train(input, targets, model, loss_function, optimizer)
         print('Saving model')
-        save(model, args.work_dir)
+        save(model, char_to_idx, idx_to_char, args.work_dir)
 
+        
+
+        print('Loading test data from {}'.format(args.test_data))
+        test_data = load_test_data(args.test_data)
+        print('Making predictions')
+        rnn_preds = evaluate(test_data, model, char_to_idx, idx_to_char)
+        print('Writing predictions to {}'.format(args.test_output))
+        write_pred(rnn_preds, args.test_output)
+    elif args.mode == "dev":
+        idx_to_char, char_to_idx = load_dictionary(args.work_dir)
+        model = LSTMGenerator(EMBEDDING_DIM, HIDDEN_DIM, len(char_to_idx), len(char_to_idx)).to(device)
+        load(model, args.work_dir)
         print('Loading test data from {}'.format(args.test_data))
         test_data = load_test_data(args.test_data)
         print('Making predictions')
